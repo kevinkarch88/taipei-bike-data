835f455aec96edbba780fa4495cfaeced65d9350
kevinkarch88 <kevinkarch88@gmail.com>
2024-09-03 14:17:55 -0400

    removed dagster references

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from dagster import op, job, schedule, in_process_executor, Definitions
 import logging
 from dateutil import parser
 import datetime
@@ -15,20 +14,18 @@ os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
 
-@op
-def fetch_bike_data(context):
+def fetch_bike_data():
     url = "https://api.citybik.es/v2/networks/youbike-new-taipei"
     response = requests.get(url)
     if response.status_code == 200:
-        context.log.info("Data fetched successfully.")
+        logging.info("Data fetched successfully.")
         return response.json()
     else:
-        context.log.error(f"Failed to fetch data. HTTP Status Code: {response.status_code}")
+        logging.error(f"Failed to fetch data. HTTP Status Code: {response.status_code}")
         return None
 
-# Get fact table with slots and check if new stations
-@op
-def process_bike_data(context, data, cached_dim_data):
+
+def process_bike_data(data, cached_dim_data):
     spark = SparkSession.builder.appName("BikeStationDataProcessing").getOrCreate()
 
     fact_schema = StructType([
@@ -58,8 +55,8 @@ def process_bike_data(context, data, cached_dim_data):
         timestamp = parser.isoparse(timestamp_str) if timestamp_str else datetime.datetime.utcnow()
         timestamp = timestamp.replace(second=0, microsecond=0)
 
-        free_bikes = max(0, min(station.get("free_bikes", 0), 255))
-        empty_slots = max(0, min(station.get("empty_slots", 0), 255))
+        free_bikes = station.get("free_bikes", 0)
+        empty_slots = station.get("empty_slots", 0)
 
         fact_data_list.append((station_id, timestamp, free_bikes, empty_slots))
 
@@ -78,7 +75,7 @@ def process_bike_data(context, data, cached_dim_data):
             ))
 
             # Log the new station
-            context.log.info(f"New station found: {english_name} at {datetime.datetime.utcnow()}")
+            logging.info(f"New station found: {english_name} at {datetime.datetime.utcnow()}")
 
     # Convert lists to DataFrames
     fact_df = spark.createDataFrame(fact_data_list, schema=fact_schema)
@@ -92,7 +89,7 @@ def process_bike_data(context, data, cached_dim_data):
             .option("header", "true") \
             .mode("overwrite") \
             .save(dim_filename)
-        context.log.info(f"Dimension table CSV saved successfully as {dim_filename}")
+        logging.info(f"Dimension table CSV saved successfully as {dim_filename}")
 
     # Save fact DataFrame to CSV
     fact_filename = f"fact_bike_availability_{datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')}.csv"
@@ -101,46 +98,30 @@ def process_bike_data(context, data, cached_dim_data):
         .option("header", "true") \
         .mode("overwrite") \
         .save(fact_filename)
-    context.log.info(f"Fact table CSV saved successfully as {fact_filename}")
+    logging.info(f"Fact table CSV saved successfully as {fact_filename}")
 
     return fact_filename
 
 
-# Load existing dimension data from CSV, WIP csv name
-@op
-def load_cached_dim_data(context):
+def load_cached_dim_data():
     if os.path.exists("dim_stations.csv"):
         spark = SparkSession.builder.appName("BikeStationDataProcessing").getOrCreate()
         dim_df = spark.read.format("csv").option("header", "true").load("dim_stations.csv")
         cached_dim_data = set(dim_df.select("id").rdd.flatMap(lambda x: x).collect())
-        context.log.info(f"Loaded {len(cached_dim_data)} station IDs from the dimension table.")
+        logging.info(f"Loaded {len(cached_dim_data)} station IDs from the dimension table.")
     else:
         cached_dim_data = set()
-        context.log.info("No existing dimension data found. Starting fresh.")
+        logging.info("No existing dimension data found. Starting fresh.")
 
     return cached_dim_data
 
 
-# Steps of execution for dagster
-@job(executor_def=in_process_executor)
-def bike_pipeline():
+def main():
     data = fetch_bike_data()
-    cached_dim_data = load_cached_dim_data()
-    process_bike_data(data, cached_dim_data)
-
-
-# Schedule pipeline to run every minute
-@schedule(cron_schedule="*/1 * * * *", job=bike_pipeline, execution_timezone="UTC")
-def minute_schedule(_context):
-    return {}
-
-
-# definitions needed to get scheduler to work in UI
-defs = Definitions(
-    jobs=[bike_pipeline],
-    schedules=[minute_schedule]
-)
+    if data:
+        cached_dim_data = load_cached_dim_data()
+        process_bike_data(data, cached_dim_data)
 
 
 if __name__ == "__main__":
-    result = bike_pipeline.execute_in_process()
+    main()
