ac1165ff07f6aa56815e281dad9327bec7d4308e
kevinkarch88 <kevinkarch88@gmail.com>
2024-09-10 15:16:05 -0400

    first dbt query and test

---
+++ dbt/models/average_usage.sql
@@ -0,0 +1,38 @@
+WITH usage_data AS (
+    SELECT
+        fact.station_id,
+        fact.timestamp,
+        fact.free_bikes,
+        fact.empty_slots,
+        dim.name AS station_name,
+        CASE
+            WHEN (fact.free_bikes + fact.empty_slots) = 0 THEN 0
+            ELSE (fact.empty_slots / (fact.free_bikes + fact.empty_slots)) * 100
+        END AS bike_usage_percentage
+    FROM
+        `taipei-bike-data-project.bike_big_query.fact_table` AS fact
+    JOIN
+        `taipei-bike-data-project.bike_big_query.dim_table` AS dim
+    ON
+        fact.station_id = dim.station_id
+)
+, average_usage AS (
+    SELECT
+        station_id,
+        station_name,
+        AVG(bike_usage_percentage) AS avg_usage_percentage
+    FROM
+        usage_data
+    GROUP BY
+        station_id,
+        station_name
+)
+SELECT
+    station_id,
+    station_name,
+    avg_usage_percentage
+FROM
+    average_usage
+ORDER BY
+    avg_usage_percentage DESC
+LIMIT 10000
