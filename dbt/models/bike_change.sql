463f2fc2dce55078ed30a8253ca9e9be76440a1f
kevinkarch88 <kevinkarch88@gmail.com>
2024-09-11 15:01:41 -0400

    forgot to write the query in the file

---
+++ dbt/models/bike_change.sql
@@ -0,0 +1,37 @@
+WITH bike_change AS (
+    SELECT
+        fact.station_id,
+        dim.name AS station_name,
+        fact.timestamp,
+        fact.free_bikes,
+        LAG(fact.free_bikes) OVER (PARTITION BY fact.station_id ORDER BY fact.timestamp) AS previous_free_bikes,
+        (fact.free_bikes - LAG(fact.free_bikes) OVER (PARTITION BY fact.station_id ORDER BY fact.timestamp)) AS change_in_free_bikes
+    FROM
+        `taipei-bike-data-project.bike_big_query.fact_table` AS fact
+    JOIN
+        `taipei-bike-data-project.bike_big_query.dim_table` AS dim
+    ON
+        fact.station_id = dim.station_id
+)
+, average_change AS (
+    SELECT
+        station_id,
+        station_name,
+        AVG(ABS(change_in_free_bikes)) AS avg_change_in_free_bikes
+    FROM
+        bike_change
+    WHERE
+        previous_free_bikes IS NOT NULL
+    GROUP BY
+        station_id,
+        station_name
+)
+SELECT
+    station_id,
+    station_name,
+    avg_change_in_free_bikes
+FROM
+    average_change
+ORDER BY
+    avg_change_in_free_bikes DESC
+LIMIT 1000
